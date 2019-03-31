dc48fd9646a4e63c503d03ec3d85c9f20845d501
Juan Sobral <juan.sobral@usp.br>
2019-03-31 14:15:43 -0300

    Data adicionada e organização geral da aparência

--- cambio.py
+++ cambio.py
@@ -1,19 +1,33 @@
+print("")
+print("")
+print("### CAMBIO DE MOEDAS EM TEMPO REAL ###")
+print("")
+
 import requests
 import json
 
 url = "http://data.fixer.io/api/latest?access_key=4269a27be188915e860139581dbb8789"
 print("Acessando base de dados...")
+
 response = requests.get(url)
 print(response)
+
 if response .status_code == 200:
+    
     print("Consegiu acessar base de dados!")
     print("Buscando informações das moedas...")
+    
     dados = response.json()
+    day = dados['date']
+    
     euro_real = dados['rates']['BRL'] / dados['rates']['EUR']
     dollar_real = dados['rates']['BRL'] / dados['rates']['USD']
     bitcoin_real = dados['rates']['BRL'] / dados['rates']['BTC']
-    print("1 Euro = R$ " "%.2f" % euro_real)
-    print("1 Dollar = R$ ""%.2f" % dollar_real)
+    
+    print("")
+    print("Dados do dia: %s/%s/%s" % (day[8:], day[5:7], day[0:4]))
+    print("1 Euro =    R$ " "%.2f" % euro_real)
+    print("1 Dollar =  R$ ""%.2f" % dollar_real)
     print("1 Bitcoin = R$ ""%.2f" % bitcoin_real)
 else:
     print("Site com problemas!")
